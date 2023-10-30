1d3178250dbb135286d1a728c38f0f6a504805d3
EduardoGarciaEnriquez <garciaenriquez.carloseduardo@gmail.com>
2023-10-30 14:05:30 -0600

    feat: logout

--- src/data/user.ts
+++ src/data/user.ts
@@ -1,12 +1,10 @@
 import { useQueryClient, useMutation, useQuery } from '@tanstack/react-query'
 import Cookies from 'js-cookie'
 import { toast } from 'react-toastify'
-import { useRouter } from 'next/router'
 
 import { API_ENDPOINTS } from './client/api-endpoints'
 import { userClient } from './client/user'
 import { AUTH_CRED } from '@/utils/constants'
-import { Routes } from '@/config/routes'
 import { UsersResponse } from '@/types/users'
 
 export function useLogin() {
@@ -18,9 +16,7 @@ export const useMeQuery = () => {
 }
 
 export const useLogoutMutation = () => {
-  const router = useRouter()
   Cookies.remove(AUTH_CRED)
-  router.replace(Routes.login)
   toast.success('Se cerró sesión.')
   return {
     isSuccess: true,
