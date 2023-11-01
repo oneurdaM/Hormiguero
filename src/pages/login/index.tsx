5597091ba2e00f85e5545c7750a55fd0fe80e1c0
David Jimenez <david.jimenez@sissadigital.com>
2023-11-01 11:34:59 -0600

    Update: Change names from pages to index

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -14,24 +14,24 @@ function Login() {
         router.replace('/') //redirect to '/'
     }
 
-  return (
-    <div className="flex h-screen flex-row" dir="ltr">
-        <div className="hidden flex-1 items-center justify-center bg-dark text-white md:flex  ">
-          <div className=" w-auto max-w-[700px]">
-          <Image src={logo} alt="logo-bw" loading="eager" />
-          </div>
-        </div>
-
-        <div className="flex flex-1 items-center justify-center bg-gray-400 md:bg-gray-100 dark:bg-gray-700 ">
-          <div className="m-auto w-full max-w-[420px] rounded p-5 sm:p-8 md:shadow md:bg-light">
-            <div className="flex h-[100px] items-center justify-center md:text-dark">
-              Iniciar sesión
+    return (
+        <div className="flex h-screen flex-row" dir="ltr">
+            <div className="hidden flex-1 items-center justify-center bg-dark text-white md:flex  ">
+                <div className=" w-auto max-w-[700px]">
+                    <Image src={logo} alt="logo-bw" loading="eager" />
+                </div>
             </div>
-            <LoginForm />
-          </div>
+
+            <div className="flex flex-1 items-center justify-center bg-gray-400 md:bg-gray-100 dark:bg-gray-700 ">
+                <div className="m-auto w-full max-w-[420px] rounded p-5 sm:p-8 md:shadow md:bg-light">
+                    <div className="flex h-[100px] items-center justify-center md:text-dark">
+                        Iniciar sesión
+                    </div>
+                    <LoginForm />
+                </div>
+            </div>
         </div>
-      </div>
-  )
+    )
 }
 
 export default Login
