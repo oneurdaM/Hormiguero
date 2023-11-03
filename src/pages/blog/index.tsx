9ac96c971e6bd09dfc4303d771e5655cf9779995
EduardoGarciaEnriquez <garciaenriquez.carloseduardo@gmail.com>
2023-11-03 09:11:59 -0600

    change: layout

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,29 +1,21 @@
 'use client'
-
 import React, { useEffect, useState } from 'react'
-import BlogList from '@/components/blog/BlogList'
-import { Row, Col } from 'antd'
-import SiteHeader from '@/(client-components)/(Header)/SiteHeader'
-import FooterNav from '@/components/FooterNav'
-import Footer from '@/components/Footer'
-const Blog: React.FC = () => {
-    const [mounted, setMounted] = useState(false)
-    useEffect(() => {
-        setMounted(true)
-    }, [])
 
-    if (!mounted) return <></>
-    return (
-        <Row id="blog" justify="space-around">
-            <Col span={24}>
-                <SiteHeader />
-                <br />
-                <BlogList />
-                <FooterNav />
-                <Footer />
-            </Col>
-        </Row>
-    )
+import BlogList from '@/components/blog/BlogList'
+import Layout from '@/components/layout/layout'
+
+const Blog: React.FC = () => {
+  const [mounted, setMounted] = useState(false)
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  if (!mounted) return <></>
+  return (
+    <Layout>
+      <BlogList />
+    </Layout>
+  )
 }
 
 export default Blog
