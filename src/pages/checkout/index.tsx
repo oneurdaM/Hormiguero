b247d3579ea4cb31c96298e2f1c753a333814e82
EduardoGarciaEnriquez <garciaenriquez.carloseduardo@gmail.com>
2023-11-06 09:41:56 -0600

    ci: checkout

---
+++ src/pages/checkout/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import Layout from '@/components/layout/layout'
+function Checkout() {
+  return (
+    <Layout>
+      <div className="h-auto min-h-screen container py-5">checkout</div>
+    </Layout>
+  )
+}
+
+export default Checkout
