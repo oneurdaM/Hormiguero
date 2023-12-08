314340c9067746ea43be88c0e61ee8477c1bcfd9
Sebastian <sebastian_a9@hotmail.com>
2023-12-08 13:13:54 -0600

    fix: register of users

--- src/components/auth/sign-up-form.tsx
+++ src/components/auth/sign-up-form.tsx
@@ -3,6 +3,8 @@ import React, { useState } from 'react'
 import { useRouter } from 'next/navigation'
 
 import * as yup from 'yup'
+import { Drawer, Row, Col, notification, Divider, Card, Badge, Steps, Avatar, Tooltip } from 'antd'
+import { SmileOutlined } from '@ant-design/icons'
 
 import Form from '@/components/ui/forms/form'
 import Input from '@/components/ui/input'
@@ -15,93 +17,78 @@ import { useSignUpMutation } from '@/data/user'
 import FacebookButton from './login/FacebookButton'
 
 const signUpFormSchema = yup.object().shape({
-  firstName: yup.string().required('Esta campo es obligatorio'),
-  middleName: yup.string().nullable(),
-  lastName: yup.string().required('Este campo es obligatorio'),
-  birthDate: yup.string().required('Este campo es obligatorio'),
-  email: yup
-    .string()
-    .email('Correo electrónico no válido')
-    .required('Este campo es obligatorio'),
-  password: yup.string().required('Este campo es obligatorio'),
-  passwordConfirmation: yup
-    .string()
-    .required('Este campo es obligatorio')
-    .oneOf([yup.ref('password'), null], 'Las contraseñas deben de coincidir'),
+    firstName: yup.string().required('Esta campo es obligatorio'),
+    lastName: yup.string().required('Este campo es obligatorio'),
+    email: yup.string().email('Correo electrónico no válido').required('Este campo es obligatorio'),
+    password: yup.string().required('Este campo es obligatorio'),
+    passwordConfirmation: yup
+        .string()
+        .required('Este campo es obligatorio')
+        .oneOf([yup.ref('password'), null], 'Las contraseñas deben de coincidir'),
 })
 
 const SignUpForm = () => {
-  const Router = useRouter()
-  const [errorMessage, setErrorMessage] = useState<string | null>(null)
-  const { mutate: signUp, isLoading, error } = useSignUpMutation()
+    const Router = useRouter()
+    const [errorMessage, setErrorMessage] = useState<string | null>(null)
+    const [api, contextHolder] = notification.useNotification()
 
-  let currentDate = new Date()
-  currentDate.setFullYear(currentDate.getFullYear() - 18)
-  const minAge = currentDate.toISOString().split('T')[0]
+    const { mutate: signUp, isLoading, error } = useSignUpMutation()
 
-  function onSubmit({
-    email,
-    password,
-    firstName,
-    lastName,
-  }: UserRegistration) {
-    signUp(
-      {
-        email,
-        username: email,
-        password,
-        firstName,
-        lastName,
-        role: 'USER',
-        // birthDate: `${birthDate}T00:00:00.000Z`,
-        middleName: '',
-      },
-      {
-        onSuccess: () => {
-          Router.push('/login')
-        },
-        onError: () => {
-          const messages = error as any
-          if (messages?.response?.data?.message) {
-            setErrorMessage(
-              messages?.response?.data?.message !== 'Unauthorized'
-                ? messages?.response?.data?.message
-                : 'El usuario no se encuentra registrado.'
-            )
-          } else {
-            setErrorMessage('El correo y/o la contraseña están equivocados.')
+    let currentDate = new Date()
+    currentDate.setFullYear(currentDate.getFullYear() - 18)
+    const minAge = currentDate.toISOString().split('T')[0]
+
+    function onSubmit({ email, password, firstName, lastName }: UserRegistration) {
+       
+        signUp(
+          {
+            email,
+            username: email,
+            password,
+            firstName,
+            lastName,
+            role: 'USER',
+            // birthDate: `${birthDate}T00:00:00.000Z`,
+            middleName: '',
+          },
+          {
+            onSuccess: () => {
+              api.open({
+                message:  <span className='font-bold'>¡Registro exitoso!</span>,
+                description: 'Ya puedes iniciar sesión.',
+                icon: <SmileOutlined style={{ color: '#009f7f' }} />,
+                duration: 5
+            })
+
+            setTimeout(() => {
+              Router.push('/login');
+            }, 3000);
+            },
+            onError: () => {
+              const messages = error as any
+              if (messages?.response?.data?.message) {
+                setErrorMessage(
+                  messages?.response?.data?.message !== 'Unauthorized'
+                    ? messages?.response?.data?.message
+                    : 'El usuario no se encuentra registrado.'
+                )
+              } else {
+                setErrorMessage('El correo y/o la contraseña están equivocados.')
+              }
+            },
           }
-        },
-      }
-    )
-  }
+        )
+    }
 
-  return (
-    <>
-      <Form validationSchema={signUpFormSchema} onSubmit={onSubmit}>
-        {({ register, formState: { errors } }) => (
-          <>
-            <Input
-              link={Routes.login}
-              linkText="¿Ya estás registrado? Inicia sesión"
-              label="Correo electrónico"
-              placeholder="Correo electrónico"
-              {...register('email')}
-              type="email"
-              variant="outline"
-              className="mb-4"
-              error={errors?.email?.message}
-            />
-            <Input
-              label="Nombre"
-              placeholder="Nombre"
-              {...register('firstName')}
-              type="email"
-              variant="outline"
-              className="mb-4"
-              error={errors?.firstName?.message}
-            />
-            {/* <Input
+    return (
+        <>
+            {contextHolder}
+            <Form validationSchema={signUpFormSchema} onSubmit={onSubmit}>
+                {({ register, formState: { errors } }) => (
+                    <>
+                        <Input link={Routes.login} linkText="¿Ya estás registrado? Inicia sesión" label="Correo electrónico" placeholder="Correo electrónico" {...register('email')} type="email" variant="outline" className="mb-4" error={errors?.email?.message} />
+                        <Input label="Nombre" placeholder="Nombre" {...register('firstName')} type="email" variant="outline" className="mb-4" error={errors?.firstName?.message} />
+                        {/* <Input
               label="Segundo nombre (opcional)"
               placeholder="Nombre"
               {...register('middleName')}
@@ -110,16 +97,8 @@ const SignUpForm = () => {
               className="mb-4"
               error={errors?.middleName?.message}
             /> */}
-            <Input
-              label="Apellido(s)"
-              placeholder="Apellido(s)"
-              {...register('lastName')}
-              type="email"
-              variant="outline"
-              className="mb-4"
-              error={errors?.lastName?.message}
-            />
-            {/* <Input
+                        <Input label="Apellido(s)" placeholder="Apellido(s)" {...register('lastName')} type="email" variant="outline" className="mb-4" error={errors?.lastName?.message} />
+                        {/* <Input
               type="date"
               label="Fecha de nacimiento"
               {...register('birthDate')}
@@ -128,45 +107,19 @@ const SignUpForm = () => {
               max={minAge}
               error={errors.birthDate?.message?.toString()}
             /> */}
-            <PasswordInput
-              label="Contraseña"
-              placeholder="Contraseña"
-              {...register('password')}
-              error={errors?.password?.message}
-              variant="outline"
-              className="mb-4"
-            />
-            <PasswordInput
-              label="Confirmar contraseña"
-              placeholder="Contraseña"
-              {...register('passwordConfirmation')}
-              error={errors?.passwordConfirmation?.message}
-              variant="outline"
-              className="mb-4"
-            />
-            <Button
-              className="w-full bg-dark text-light hover:bg-gray-700"
-              loading={isLoading}
-              disabled={isLoading}
-            >
-              Registrarme
-            </Button>
-            <hr className="my-4" />
-            {/* <FacebookButton action={onSubmit} actionType="sign-up" /> */}
-          </>
-        )}
-      </Form>
-      {errorMessage ? (
-        <Alert
-          message={errorMessage}
-          variant="error"
-          closeable={true}
-          className="mt-5"
-          onClose={() => setErrorMessage(null)}
-        />
-      ) : null}
-    </>
-  )
+                        <PasswordInput label="Contraseña" placeholder="Contraseña" {...register('password')} error={errors?.password?.message} variant="outline" className="mb-4" />
+                        <PasswordInput label="Confirmar contraseña" placeholder="Contraseña" {...register('passwordConfirmation')} error={errors?.passwordConfirmation?.message} variant="outline" className="mb-4" />
+                        <Button className="w-full bg-dark text-light hover:bg-gray-700" loading={isLoading} disabled={isLoading}>
+                            Registrarme
+                        </Button>
+                        <hr className="my-4" />
+                        {/* <FacebookButton action={onSubmit} actionType="sign-up" /> */}
+                    </>
+                )}
+            </Form>
+            {errorMessage ? <Alert message={errorMessage} variant="error" closeable={true} className="mt-5" onClose={() => setErrorMessage(null)} /> : null}
+        </>
+    )
 }
 
 export default SignUpForm
